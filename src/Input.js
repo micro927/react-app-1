c4e9ae5a60c7c391d3be474b9654b546462c84d3
micro927 <nsbelieve-15@hotmail.com>
2022-06-29 17:16:06 +0700

    learn day 1

---
+++ src/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Input() {
+    return (
+        <div className='Input'>
+            <div className='Input__header'>Create Post</div>
+            <input
+                className='Input__field'
+                type="text"
+            />
+        </div>
+    )
+}
+
+export default Input
