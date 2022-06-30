6658441a0a6d1ff44aa76d179b8af6558cfe9517
micro927 <nsbelieve-15@hotmail.com>
2022-06-30 22:37:00 +0700

    end Course

--- src/App.js
+++ src/App.js
@@ -16,11 +16,17 @@ function App() {
     setPosts([newPost, ...posts])
     id += 1;
   }
+
+  function deletePost(id) {
+    const updatedPosts = posts.filter((post) => post.id !== id)
+    setPosts(updatedPosts)
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Input addPost={addPost} />
-      {posts.map((post) => <Post id={post.id} title={post.title} />)}
+      {posts.map((post) => <Post key={post.id} id={post.id} title={post.title} deletePost={deletePost} />)}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,17 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 
 
-function Post({ id, title }) {
-    <div className="Post">
-        asdssadsaddsa
-        <div className="Post__title">{title}</div>
-        <img className="Post__image" src={`https://source.unsplash.com/random?sig=${id}`} />
-    </div>
+function Post({ id, title, deletePost }) {
+
+    return (
+        <div className="Post">
+            <button className="Post__delete" onClick={() => deletePost(id)}>X</button>
+            <div className="Post__title">{title}</div>
+            <img className="Post__image" alt="" src={`https://source.unsplash.com/random?sig=${id}`} />
+        </div>
+    )
 }
 
 Post.propTypes = {
     id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    deletePost: PropTypes.func.isRequired
+
 }
 
 export default Post
